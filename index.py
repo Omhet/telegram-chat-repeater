e121a3c3d9df82f8e1aff38843e189d291e15b9e
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-02-27 18:48:24 +0300

    use session string

--- index.py
+++ index.py
@@ -1,24 +1,26 @@
 from telethon import TelegramClient, events, sync
+from telethon.sessions import StringSession
 from decouple import config
 
 API_ID = config('API_ID')
 API_HASH = config('API_HASH')
-SESSION_NAME = config('SESSION_NAME')
+SESSION_STRING = config('SESSION_STRING')
 CHAT_FROM = int(config('CHAT_FROM'))
 CHAT_TO = int(config('CHAT_TO'))
 
-client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
+client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
 async def main():
     async for dialog in client.iter_dialogs():
         print(dialog.name, 'has ID', dialog.id)
+        # print(client.session.save())
 
-# with client:
-#     client.loop.run_until_complete(main())
+with client:
+    client.loop.run_until_complete(main())
 
-@client.on(events.NewMessage(chats=CHAT_FROM))
-async def my_event_handler(event):
-    await client.send_message(CHAT_TO, event.raw_text)
+# @client.on(events.NewMessage(chats=CHAT_FROM))
+# async def my_event_handler(event):
+#     await client.send_message(CHAT_TO, event.raw_text)
 
-client.start()
-client.run_until_disconnected()
+# client.start()
+# client.run_until_disconnected()
