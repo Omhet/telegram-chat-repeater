e198319b704fa8a0bcd8f254a0bd84da43f0571d
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-05-11 13:09:23 +0500

    Prin session

--- index.py
+++ index.py
@@ -11,12 +11,13 @@ CHAT_TO = int(config('CHAT_TO'))
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
 async def main():
-    async for dialog in client.iter_dialogs():
-        print(dialog.name, 'has ID', dialog.id)
+    print(client.session.save())
+    # async for dialog in client.iter_dialogs():
+        # print(dialog.name, 'has ID', dialog.id)
         # print(client.session.save())
 
-# with client:
-#     client.loop.run_until_complete(main())
+with client:
+    client.loop.run_until_complete(main())
 
 @client.on(events.NewMessage(chats=CHAT_FROM))
 async def my_event_handler(event):
